1b17f42940559d379ce2bd50664790d7802b1f2f
SanyamPunia <lewarends@gmail.com>
2025-04-09 00:04:34 +0530

    fix: :zap:

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: ["/"],
       disallow: "/private/",
     },
-    sitemap: "https://www.sonancemedia.in",
+    sitemap: "https://www.sonancemedia.in/sitemap.xml",
   };
 }
